ae86019d21311eb630d4e20e645a524b2361c268
Kasukee <kanebenjamin17@gmail.com>
2022-05-20 12:11:30 -0500

    ✨data vis for par/eagle/bogetetc for a course

--- src/dg_util.py
+++ src/dg_util.py
@@ -3,6 +3,10 @@ from tkinter import *
 import json
 import os
 from tkinter import messagebox
+import matplotlib.pyplot as plt; plt.rcdefaults()
+import numpy as np
+import matplotlib.pyplot as plt
+import os
 
 #Find data file:
 filename = 'course_data.json' 
@@ -33,9 +37,57 @@ def create_course_entry(window, course, course_data, entry_data):
                 if course_data[i]["course"] == course:
                     course_data[i]["scores"].append(entry_data)
             json.dump(course_data, file_)
-
+        messagebox.showinfo("Info", "Data successfully added for {0}!".format(course))
+        
 def view_score(course):
-    pass
+    print("In view_score")
+    objects = ["Ace", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "3+"]
+    object_map = {"Ace": 0, "Eagle": 0, "Birdie": 0, "Par": 0, "Bogey": 0, "Double Bogey": 0, "3+": 0}
+    with open(filename, "r") as file_:
+        data = json.load(file_)
+        for course_ in data:
+            print("in course for")
+            print("course")
+            print(course)
+            print(course_)
+            if course == course_["course"]:
+                par_list = course_["par"]
+                for score_list in course_["scores"]:
+                    print("in score list for")
+                    index = 0
+                    for score in score_list:
+                        print("Score Here: " + str(score))
+                        print(score)
+                        print(score_list)
+                        result = score - par_list[index]
+                        print(result)
+                        if score == 1:
+                            object_map["Ace"] += 1
+                        elif score == -2:
+                            object_map["Eagle"] += 1
+                        elif result == -1:
+                            object_map["Birdie"] += 1
+                        elif result == 0:
+                            object_map["Par"] += 1
+                        elif result == 1:
+                            object_map["Bogey"] += 1
+                        elif result == 2:
+                            object_map["Double Bogey"] += 1
+                        elif result > 2:
+                            object_map["3+"] += 1
+                        index += 1
+    print(object_map)
+    print("GRAPH STUFF HAPPENING")
+    
+    y_pos = np.arange(len(objects))
+    stats = [object_map["Ace"], object_map["Eagle"], object_map["Birdie"], object_map["Par"], object_map["Bogey"], object_map["Double Bogey"], object_map["3+"]]
+
+    plt.barh(y_pos, stats, align='center', alpha=0.5)
+    plt.yticks(y_pos, objects)
+    plt.xlabel("Course Data")
+    plt.title("{0} course stats".format(course))
+    plt.show()
+    
         
 
 
